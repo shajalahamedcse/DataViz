37a098b36b390fedd96dec42c66a36ce8175014b
shajalAhamed <shajalahamedcse@gmail.com>
2017-11-05 08:45:39 +0600

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 url = 'http://192.168.0.1/awp/SFF%20PW%20Treatment%20Plant/io.html';
-const Sensor_data = new Mongo.Collection("sensor_data");
+//const Sensor_data = new Mongo.Collection("sensor_data");
 Meteor.startup(() => {
     // code to run on server at startup
     //var i = 0;\
